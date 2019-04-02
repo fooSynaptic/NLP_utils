0cc38e6a92f916e9c97f7815ac1c35ab6ff73c61
hujiaxin <hujiaxin@ajmide.com>
2019-04-02 17:57:58 +0800

    fast sort updated

--- fast_sort.py
+++ fast_sort.py
@@ -10,14 +10,14 @@ def quicksort(arr, p, r):
 
 
 def my_PARTITION(arr, p, r):
-	x = arr[r-1]
-	i = p-1
+	x,i = arr[r], p-1
+	#i = p-1
 
-	for j in range(p, r-1):
+	for j in range(p, r):
 		if arr[j] <= x:
 			i = i + 1
 			arr[i], arr[j] = arr[j], arr[i]			
-	arr[r-1], arr[i+1] = arr[i+1], arr[r-1]
+	arr[r], arr[i+1] = arr[i+1], arr[r]
 	
 	return i+1
 
@@ -27,6 +27,10 @@ def my_PARTITION(arr, p, r):
 
 arr = [2,8,7,1,3,5,6,4]
 
-quicksort(arr, 0, 8)
+quicksort(arr, 0, 7)
 
 print(arr)
+
+arr2 = [2,5,4,6,7,9,10,1]
+quicksort(arr2, 0, 7)
+print(arr2)

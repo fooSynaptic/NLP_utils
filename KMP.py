b44c9200b1c2deb1ef93df172c92b2220958c7a2
hujiaxin <hujiaxin@ajmide.com>
2019-09-11 10:55:22 +0800

    update

--- KMP.py
+++ KMP.py
@@ -12,7 +12,7 @@ def common_len(pre, suff):
 
 
 
-def KMP(S, p):
+def KMP(S, p, verbose = False):
     sub_p = [p[:i] for i in range(1, len(p))]
     sub_p.append(p)
 
@@ -33,22 +33,23 @@ def KMP(S, p):
     #start mapping
     i, j = 0, 0
     while i < len(S) and j < len(p):
-        print(S)
-        print(' '*i + p)
-        print(' '*i + '^')
+        if verbose:
+            print(S)
+            print(' '*i + p)
+            print(' '*i + '^')
 
         if S[i] == p[j] or j == -1:
-            print('incre i and j')
+            if verbose: print('incre i and j')
             i += 1
             j += 1
-            print('after incre', i, j)
+            if verbose: print('after incre', i, j)
         else:
-            print("Move next...")
+            if verbose: print("Move next...")
             j = move_next[j]
-            print(i, j)
+            if verbose: print(i, j)
         
         if j == len(p):
-            print('The mapping stirng of S refer to p start from ', i-j)
+            print('The mapping stirng of S refer to p start from ', i-j, i, j)
             return i-j
     print("No mapping...")
     return -1
@@ -62,4 +63,16 @@ def KMP(S, p):
 S = 'acababaabcacabc'
 p = 'abaabcac'
 
-KMP(S, p)
+KMP(s, p, verbose = True)
+
+
+def mapping():
+    #We want map p from s
+    l1, l2 = len(s), len(p)
+    for i in range(l2-1, -1, -1):
+        res = KMP(s, p[:i])
+        if res > 0:
+            print(p[:i])
+
+
+mapping()

8fea18e5ead9450290574dfb5f3e81bd887a88b5
hujiaxin <hujiaxin@ajmide.com>
2019-10-16 12:48:45 +0800

    update crf implement from scratch

---
+++ ML/PGM/CRF/crf_from_scratch/models.py
@@ -0,0 +1,195 @@
+# encoding=utf-8
+# /usr/bin/python3
+
+"""crf implementation from scratch"""
+
+import numpy as np
+from collections import defaultdict
+from numpy import empty, zeros, ones, log, exp, add, int32
+from numpy.random import uniform
+from utils import *
+import os
+
+from sklearn.externals import joblib
+from tqdm import tqdm
+
+
+
+
+
+class linearChainCRF():
+    def __init__(self, xnode, ynode):
+        """
+        K: the length of labels
+        M: the length of features
+        """
+        self.K = len(ynode)              # size of target label
+        self.M = len(xnode)	        # total feature dimension length
+        self.featureNodes, self.labelNodes = xnode, ynode
+        size = (self.K + self.M) * self.K
+        self.W = np.zeros(size)
+        print("feature dimension: ", self.W.shape)
+
+    
+    def currentField(self, x):
+        """calculate potential parameter given current obeservation data. """
+        """x is a taggedFeatureTable object"""
+        N, K, W = x.seq.__len__(), self.K, self.W
+
+        # estimate
+        g0 = empty(K)
+        g = empty((N-1, K, K))    #at timestep n, the logit of i-th label transfer to j-th label
+
+        for y in range(K):
+            # at step 0, the logit of label k (from time -1)
+            g0[y] = W[x[0, None, y]].sum()
+
+        
+        for t in range(1, N):
+            for y in range(K):
+                for yp in range(K):
+                    g[t-1,yp,y] = W[x[t,yp,y]].sum()
+
+        return g0, g
+
+
+    
+    def vertebi_inference(self, x):
+        """sequence labeling, inference the most likely label given x"""
+        """x is a taggedFeatureTable object"""
+        N, K = x.seq.__len__(), self.K
+        g0, g = self.currentField(x)
+        B = ones((N, K), dtype=int32) * -1
+
+        # compute max-marginals and backtrace matrix
+        V = g0
+        for t in range(1, N):
+            U = empty(K)            # at time step t, only record the maxlogit pre label
+            for y in range(K):
+                w = V + g[t-1,:,y]  # at time t, the logit of all labels in t-1 transfer to label y
+                B[t,y] = b = w.argmax()     # recore which label in time t-1 have the max logit
+                U[y] = w[b]
+            V = U
+
+        # extract the best path by brack-tracking
+        y = V.argmax()
+        trace = []
+        for t in range(N-1, -1, -1):
+            trace.append(y)
+            y = B[t, y]
+
+        return trace[::-1]
+
+
+    def logitEstimator(self, x, targetLabel):
+        """estimate label likely-hood given W"""
+        N = x.seq.__len__()
+        K = self.K
+        W = self.W
+        g0, g = self.currentField(x)
+        a = self.forward(g0, g, N, K)
+        logZ = logsumexp(a[N-1, :])
+        return sum(W[k] for k in targetLabel) - logZ
+
+
+    def forward(self, g0, g, N, K):
+        """forward stepwise"""
+        a = zeros((N, K))
+        a[0, :] = g0
+
+        for t in range(1, N):
+            yp = a[t-1, :]
+            for y in range(K):
+                a[t, y] = logsumexp(yp + g[t-1, :, y])
+        
+        return a
+
+
+    def backward(self, g, N, K):
+        """backword stepwise"""
+        b = zeros((N, K))
+
+        for t in range(N-2, -1, -1):
+            yp = b[t+1, :]
+            for y in range(K):
+                b[t, y] = logsumexp(yp + g[t, y, :])
+
+        return b
+
+
+    def Exp(self, x):
+        """expectation of x"""
+        N = x.seq.__len__()
+        W = self.W
+        K = self.K
+
+        g0, g = self.currentField(x)
+
+        a = self.forward(g0, g, N, K)
+        b = self.backward(g, N, K)
+        ### scale Z
+        logZ = logsumexp(a[N-1, :])
+
+        ans = dict()
+        # here e = exp(a + b - logZ)
+        # at step 0, a == g0 
+        e0 = exp(g0 + b[0,:] - logZ).clip(0., 1.)
+        for y in range(K):
+            prob = float(e0[y])
+            for k in x[0, None, y]:
+                # the k-pos logit from step -1 transfer to label y
+                ans[k] = ans.get(k, 0.) + prob
+
+
+        for t in range(1, N):
+            """
+            Outer: broadcast: not element-wise operation
+            """
+            # e_i = {(forward[t-1] + backward[t])*P(y|x)_t-1}/ logZ	    logit of time i
+            # or exp(a[t-1,yp] + g[t-1,yp,y] + b[t,y] - logZ)
+            ei = exp((add.outer(a[t-1,:], b[t,:]) + g[t-1,:,:] - logZ)).clip(0., 1.)
+            #ei = exp(a[t-1,:] + g[t-1,:,:] + b[t,:] - logZ).clip(0., 1.)
+            #print(ei.shape)
+            for yp in range(K):
+                for y in range(K):
+                   prob = float(ei[yp, y])
+                   for k in x[t, yp, y]:
+                       ans[k] = ans.get(k, 0.) + prob
+
+        return ans
+
+
+    def edgefeatures(self, x, y):
+        """
+        This is important
+        for our raw input t1, t2, t3, t4 ...
+        transfer the token sequence information into {f1, ... ft}1, {f1, ... ft}2
+        """
+        features = list(x[0, None, y[0]])
+        assert len(x.seq) == len(y), "seq to label not alignmentted..."
+
+        for t in range(1, x.seq.__len__()):
+            for i in x[t, y[t-1], y[t]]:
+                features.append(i)
+
+        return features
+
+
+    def sgd(self, data, iters = 20, Nwarm_up = 10, saveParam = False, modelpath = 'linearCRF'):
+        W = self.W  ## reference
+        for i in range(iters):
+            print("Epochs: ", i)
+            lr_rate = Nwarm_up/(i+1)**0.501
+            for x, labels in tqdm(data):
+                ftoken = FeatureTable(x, self.featureNodes, self.labelNodes)
+                flabel = self.edgefeatures(ftoken, [self.labelNodes[label] for label in labels])
+                for k, explogit in self.Exp(ftoken).items():
+                    W[k] -= lr_rate * explogit
+                for k in flabel:
+                    W[k] += lr_rate
+
+        if saveParam:
+            if not os.path.exists(modelpath):
+                os.mkdir(modelpath)
+            joblib.dump(self.W, os.path.join(modelpath, 'crf_W.pkl'))
+
---
+++ ML/PGM/CRF/crf_from_scratch/utils.py
@@ -0,0 +1,129 @@
+# encoding=utf-8
+# /usr/bin/python3
+from bs4 import BeautifulSoup as bs
+import bs4
+import re
+import numpy as np
+
+
+
+"""Implementation tools... """
+
+
+
+
+def tokenfeatures(word):
+    """ Obsevation state features """
+    f1 = "token==" + word
+    mid = len(word)//2
+    f2, f3 = 'Swith+'+word[:mid], 'Ewith+'+word[mid:]
+    f4 = 'abbre==' + re.sub('[0-9]', '0', re.sub('[^a-zA-Z0-9()\.\,]', '', word.lower()))
+    f5 = ["contains" + c for c in re.findall('[^a-zA-Z0-9]', word)]
+    return [f1, f2, f3, f4] + f5
+
+
+
+def Transferfeature(seq):
+    """ Transfer features """
+    seq.insert(0, ['seq_start'])
+    seq.append(['seq_end'])
+    for i in range(len(seq)):
+        seq[i].extend(tokenfeatures(seq[i][0]))
+    if True:
+        # previous token feature
+        for i in range(1, len(seq)):
+            seq[i].extend('@-1:'+f for f in seq[i-1][1:] \
+                if not (f.startswith('@+1:') or f.startswith('@-1:')))
+        # next token feature
+        for i in range(len(seq)-1):
+            seq[i].extend('@+1:'+f for f in seq[i+1][1:] \
+                if not (f.startswith('@+1:') or f.startswith('@-1:')))
+    return seq
+
+
+
+def parser(line):
+    """parse a tagged reference."""
+    lines = bs(line, features="lxml")
+    sublines = [_line for _line in lines.body.descendants if isinstance(_line, bs4.element.Tag)]
+    res = []
+    for p in sublines:
+        tag = p.name
+        for token in p.string.split():
+            res.append((tag, token))
+    return res
+
+
+
+def load_data(path, proportion=0.1):
+    """load src data given proportion"""
+    data = []
+    for line in open(path).readlines():
+        line = line.strip()
+        if line == '<NEWREFERENCE>':
+            continue
+        line = parser(line)
+        x, y = [[f[1]] for f in line], [f[0] for f in line]
+        y.insert(0, 'seqStart')
+        y.append('seqEnd')
+        instance = (Transferfeature(x), y)
+        data.append(instance)
+    return data[:int(proportion * len(data))]
+
+
+#s = load_data('./data/tagged_references.txt')[:10]
+
+
+def updateFieldNode(data):
+    """Loading feature map configure"""
+    labels, attributes = set(), set()
+    for ftoken, label in data:
+        labels.update(label)
+        attributes.update(word for line in ftoken for word in line)
+
+    attributes.update(['seqStart', 'seqEnd'])
+    label2id, attr2id = {}, {}
+    for _id, label in enumerate(labels):
+        label2id[label] = _id
+
+    for _id, attr in enumerate(attributes):
+        attr2id[attr] = _id
+    return label2id, attr2id
+
+
+def logsumexp(a):
+    """
+    Compute the log of the sum of exponentials of an array ``a``, :math:`\log(\exp(a_0) + \exp(a_1) + ...)`
+    """
+    ### introduce max(a) to avoid overflow.
+    A = a.max()
+    return A + np.log((np.exp(a-A)).sum())
+
+
+def sample(x):
+    cdf = x.cumsum()
+    Z = cdf[-1]
+    u = uniform()
+    return cdf.searchsorted(u * Z)
+
+
+
+class FeatureTable():
+    def __init__(self, tokenlist, xnode, ynode):
+        self.seq = []
+        for tokenfeatures in tokenlist:
+            # tokenfeatures[0] is the raw token, others are token attributes.
+            self.seq.append(np.array([xnode[f] for f in tokenfeatures], dtype=np.int32))
+
+        self.xdim, self.ydim = len(xnode), len(ynode)
+
+    def __getitem__(self, item):
+        t, yp, y = item
+        ftoken = self.seq[t]
+        if yp is not None:
+            return np.append(ftoken, yp) + y*self.xdim
+        else:
+            return ftoken + y*self.xdim
+
+        
+
